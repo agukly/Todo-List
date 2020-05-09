081a21ea3de498d091debd91a9ea8c432703ba98
Aga Klyz <klyz.aga@gmail.com>
2020-05-09 12:10:47 +1000

    deleting and adding task functionality

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,3 +1,18 @@
-$("li").click(function(){
+$("ul").on("click", "li", function(){
   $(this).toggleClass("completed");
+});
+
+$("ul").on("click", "span", function(event){
+  $(this).parent().fadeOut(300, function(){
+    $(this).remove();
+  });
+  event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+  if(event.which === 13){
+    var task = $(this).val();
+    $(this).val("");
+    $("ul").append("<li><span>X </span>" + task + "</li>")
+  }
 });
