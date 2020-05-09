597bdc6c8708b16e0344fe6dd637f43ed6995195
Aga Klyz <klyz.aga@gmail.com>
2020-05-09 16:20:30 +1000

    styling

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -13,6 +13,10 @@ $("input[type='text']").keypress(function(event){
   if(event.which === 13){
     var task = $(this).val();
     $(this).val("");
-    $("ul").append("<li><span>X </span>" + task + "</li>")
+    $("ul").append("<li><span>✗ </span>" + task + "</li>")
   }
 });
+
+$("h3").click(function(){
+  $("input[type='text']").fadeToggle()
+})
